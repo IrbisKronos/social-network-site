c8bc3aa09b766acaef800bc26550f3ba70f013ff
Artem Vasylenko <irbiskronos@gmail.com>
2022-07-08 14:39:53 +0300

    23.2 commit

---
+++ src/components/Profile/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import set from './Profile.module.css';
+import MyPosts from "./MyPost/MyPosts";
+
+const Profile = () => {
+    return (
+                    <div>
+                Ava + description
+            </div>
+                )
+}
+
+export default Profile;
