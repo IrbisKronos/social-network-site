5403dda58a18a43a47ca420bdc62c550d18fde72
Artem Vasylenko <irbiskronos@gmail.com>
2022-07-27 11:47:49 +0300

    dispatch(action)

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ const App = (props) => {
                             path="/"
                             element = {<Profile
                                 profilePage={props.state.profilePage}
-                                addPost={props.addPost}
-                                updateNewPostText={props.updateNewPostText}
+                                dispatch={props.dispatch}
                             />}
                         />
                         <Route
--- src/components/Profile/MyPost/MyPosts.jsx
+++ src/components/Profile/MyPost/MyPosts.jsx
@@ -11,12 +11,14 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.addPost();
+        //props.addPost();
+        props.dispatch({ type: 'ADD-POST'});
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.updateNewPostText(text);
+        //props.updateNewPostText(text);
+        props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text });
     }
 
     return (
